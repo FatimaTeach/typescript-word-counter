21b4f204e16d2a2a3076a48dd3f8eb524a03ae55
Fatima <161175945+FatimaTeach@users.noreply.github.com>
2024-06-29 16:46:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//this line is called shebang, it tells the OS to run it with node js
+import inquirer from "inquirer";
+//declare a constant'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter the content to count words",
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+//print the array of words to the console
+console.log(words);
+//print the word count of the content to the console
+console.log(`Your content word count is ${words.length}`);
